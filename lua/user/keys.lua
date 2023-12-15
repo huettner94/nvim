16b761c3571167471c8aa401e61d3be847d78c16
Felix Huettner <felix.huettner@mail.schwarz>
2023-12-15 13:49:13 +0100

    add goto commands

--- lua/user/keys.lua
+++ lua/user/keys.lua
@@ -5,6 +5,13 @@ wk.register({
     name = "file", -- optional group name
     f = { "<cmd>Telescope find_files<cr>", "Find File" },
     g = { "<cmd>Telescope live_grep<cr>", "Grep" },
-    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File"}
+    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" }
+  },
+  g = {
+    name = "goto",
+    D = { vim.lsp.buf.declaration, "Declaration" },
+    d = { vim.lsp.buf.definition, "Definition" },
+    i = { vim.lsp.buf.implementation, "Implementation" },
+    r = { vim.lsp.buf.references, "References" },
   },
 }, { prefix = "<leader>" })
