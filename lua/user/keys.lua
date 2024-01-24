3b96bc830deac3a05ce52fd9d0ab10c55e4306f9
Felix Huettner <felix.huettner@mail.schwarz>
2024-01-24 11:05:15 +0100

    more telescope cool stuff

--- lua/user/keys.lua
+++ lua/user/keys.lua
@@ -2,16 +2,21 @@ local wk = require("which-key")
 
 wk.register({
   f = {
-    name = "file", -- optional group name
+    name = "File", -- optional group name
     f = { "<cmd>Telescope find_files<cr>", "Find File" },
     g = { "<cmd>Telescope live_grep<cr>", "Grep" },
     r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" }
   },
   g = {
-    name = "goto",
+    name = "Goto",
     D = { vim.lsp.buf.declaration, "Declaration" },
     d = { vim.lsp.buf.definition, "Definition" },
     i = { vim.lsp.buf.implementation, "Implementation" },
-    r = { vim.lsp.buf.references, "References" },
+    r = { "<cmd>Telescope lsp_references<cr>", "References" },
+  },
+  r = {
+    name = "Repo",
+    c = { "<cmd>Telescope git_commits<cr>", "Commits" },
+    b = { "<cmd>Telescope git_branches<cr>", "Branches" },
   },
 }, { prefix = "<leader>" })
